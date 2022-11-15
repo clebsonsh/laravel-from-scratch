ddb4305c07cc55c18eb239cee13380f930f91785
Clebson Moura <clebson.sh@gmail.com>
2022-11-15 19:40:47 -0300

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post/my-first-post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur deserunt nihil dolorum! Vero hic dolor
+            voluptas beatae ducimus tempora amet, est numquam sapiente blanditiis animi ipsum, porro dolorum. Eveniet,
+            ab adipisci, temporibus ullam doloremque excepturi recusandae ducimus hic quo iste odit fuga architecto
+            perferendis saepe in tempora? Minima corrupti voluptatibus dolores explicabo dolorum adipisci similique
+            delectus eius, natus ratione deleniti ducimus sequi accusamus minus optio debitis omnis modi? Aperiam
+            necessitatibus cum reiciendis perspiciatis facere libero minus ipsum officia, earum quaerat fuga blanditiis
+            neque corporis exercitationem dolore nesciunt ullam deleniti accusantium est tempora hic error repudiandae?
+            Quas commodi esse quo laborum!
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post/my-second-post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur deserunt nihil dolorum! Vero hic dolor
+            voluptas beatae ducimus tempora amet, est numquam sapiente blanditiis animi ipsum, porro dolorum. Eveniet,
+            ab adipisci, temporibus ullam doloremque excepturi recusandae ducimus hic quo iste odit fuga architecto
+            perferendis saepe in tempora? Minima corrupti voluptatibus dolores explicabo dolorum adipisci similique
+            delectus eius, natus ratione deleniti ducimus sequi accusamus minus optio debitis omnis modi? Aperiam
+            necessitatibus cum reiciendis perspiciatis facere libero minus ipsum officia, earum quaerat fuga blanditiis
+            neque corporis exercitationem dolore nesciunt ullam deleniti accusantium est tempora hic error repudiandae?
+            Quas commodi esse quo laborum!
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post/my-third-post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur deserunt nihil dolorum! Vero hic dolor
+            voluptas beatae ducimus tempora amet, est numquam sapiente blanditiis animi ipsum, porro dolorum. Eveniet,
+            ab adipisci, temporibus ullam doloremque excepturi recusandae ducimus hic quo iste odit fuga architecto
+            perferendis saepe in tempora? Minima corrupti voluptatibus dolores explicabo dolorum adipisci similique
+            delectus eius, natus ratione deleniti ducimus sequi accusamus minus optio debitis omnis modi? Aperiam
+            necessitatibus cum reiciendis perspiciatis facere libero minus ipsum officia, earum quaerat fuga blanditiis
+            neque corporis exercitationem dolore nesciunt ullam deleniti accusantium est tempora hic error repudiandae?
+            Quas commodi esse quo laborum!
+        </p>
+    </article>
+</body>
+
+</html>
