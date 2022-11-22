de1cdeadb091ad99302dcdd0c9a49c94dadb9d41
Clebson Moura <clebson.sh@gmail.com>
2022-11-22 11:39:20 -0300

    Blade Layouts Two Ways

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-
-<body>
+<x-layout>
     <article>
         <h2>{{ $post->title }}</h2>
 
@@ -19,6 +8,4 @@
     </article>
 
     <a href="/">Go Back</a>
-</body>
-
-</html>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-
-<body>
+<x-layout>
     @foreach ($posts as $post)
         <article>
             <h2>
@@ -23,6 +12,4 @@
             </div>
         </article>
     @endforeach
-</body>
-
-</html>
+</x-layout>
