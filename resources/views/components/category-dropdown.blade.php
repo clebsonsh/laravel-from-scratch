d95206e73b958a2be3eaa89bdbcaf35b788be504
Clebson Moura <clebson.sh@gmail.com>
2023-04-06 14:17:55 -0300

    Extract a Category Dropdown Blade Component

---
+++ resources/views/components/category-dropdown.blade.php
@@ -0,0 +1,21 @@
+<x-dropdown>
+    <x-slot name="trigger">
+        {{ ucwords($currentCategory->name ?? 'Categories') }}
+        <svg class="transform absolute -rotate-90 pointer-events-none" style="right: 12px;" width="22" height="22"
+            viewBox="0 0 22 22">
+            <g fill="none" fill-rule="evenodd">
+                <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
+                </path>
+                <path fill="#222" d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
+                </path>
+            </g>
+        </svg>
+    </x-slot>
+
+    <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
+    @foreach ($categories as $category)
+        <x-dropdown-item href="/?category={{ $category->slug }}">
+            {{ ucwords($category->name) }}
+        </x-dropdown-item>
+    @endforeach
+</x-dropdown>
--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -6,28 +6,7 @@
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-4">
         <!--  Category -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
-            <x-dropdown>
-                <x-slot name="trigger">
-                    {{ ucwords($currentCategory->name ?? 'Categories') }}
-                    <svg class="transform absolute -rotate-90 pointer-events-none" style="right: 12px;" width="22"
-                        height="22" viewBox="0 0 22 22">
-                        <g fill="none" fill-rule="evenodd">
-                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                            </path>
-                            <path fill="#222"
-                                d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
-                            </path>
-                        </g>
-                    </svg>
-                </x-slot>
-
-                <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
-                @foreach ($categories as $category)
-                    <x-dropdown-item href="/?category={{ $category->slug }}" :active='request()->is("categories/{$category->slug}")'>
-                        {{ ucwords($category->name) }}
-                    </x-dropdown-item>
-                @endforeach
-            </x-dropdown>
+            <x-category-dropdown />
         </div>
 
         <!-- Other Filters -->
