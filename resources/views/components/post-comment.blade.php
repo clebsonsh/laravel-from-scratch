56a6191d8313bc8b17fc67e45958cba559d06310
Clebson Moura <clebson.sh@gmail.com>
2023-04-14 11:32:44 -0300

    Write the Markup for a Post Comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,20 @@
+<article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
+    <div class="flex-shrink-0">
+        <img class="rounded-full" src="https://i.pravatar.cc/100" alt="Profile" height="60" width="60">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eos quod omnis at dolorum
+            consequatur esse provident non, temporibus soluta illum doloremque inventore veniam fugit
+            vero quae deleniti porro repellendus.
+        </p>
+    </div>
+</article>
