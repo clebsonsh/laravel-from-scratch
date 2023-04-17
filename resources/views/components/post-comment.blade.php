201139b3768e8636dba2f938d3d4c47ad8022d24
Clebson Moura <clebson.sh@gmail.com>
2023-04-17 12:14:16 -0300

    Design the Comment Form

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,21 +1,23 @@
 @props(['comment'])
 
-<article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
-    <div class="flex-shrink-0">
-        <img class="rounded-full" src="https://i.pravatar.cc/100?u={{ $comment->id }}" alt="Profile" height="60"
-            width="60">
-    </div>
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img class="rounded-full" src="https://i.pravatar.cc/100?u={{ $comment->id }}" alt="Profile" height="60"
+                width="60">
+        </div>
 
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->username }}</h3>
-            <p class="text-xs">Posted
-                <time>{{ $comment->created_at->diffForHumans() }}</time>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
+                <p class="text-xs">Posted
+                    <time>{{ $comment->created_at->diffForHumans() }}</time>
+                </p>
+            </header>
+
+            <p>
+                {{ $comment->body }}
             </p>
-        </header>
-
-        <p>
-            {{ $comment->body }}
-        </p>
-    </div>
-</article>
+        </div>
+    </article>
+</x-panel>
