1cbec139ca56d4108eeca8501d82d897acfa2e63
Clebson Moura <clebson.sh@gmail.com>
2023-05-02 12:42:26 -0300

    All About Authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('admin', fn (User $user) => $user->username === 'clebson.moura');
+
+        Blade::if('admin', fn () => request()->user()?->can('admin'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,14 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PostController::class, 'index'])->name('home');
-
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 Route::get('login', [SessionController::class, 'create'])->middleware('guest')->name('login');
 Route::post('login', [SessionController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
 
+Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentController::class, 'store'])->middleware('auth');
 
-Route::resource('admin/posts', AdminPostController::class)->middleware('admin')->except('show');
+Route::resource('admin/posts', AdminPostController::class)->middleware('can:admin')->except('show');
